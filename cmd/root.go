de8a3cdc52839d049eaa3baff5159220855dcc2b
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2025-03-01 09:22:01 -0600

    refactor(cmd/root): I misunderstood how tf.Plan, it always returns something, even when there are no changes

--- cmd/root.go
+++ cmd/root.go
@@ -45,8 +45,12 @@ var (
 	cfgFile     string
 	out         io.Reader
 	flagNoColor bool
+	mdParam     string
+	planMd      string
+	planPath    string
 	planStr     string
 	sb          strings.Builder
+	sbPlan      string
 	Verbose     bool
 	Version     string
 	Date        string
@@ -57,6 +61,8 @@ var (
 	green       = color.New(color.FgHiGreen).SprintFunc()
 	yellow      = color.New(color.FgHiYellow).SprintFunc()
 	red         = color.New(color.FgHiRed).SprintFunc()
+	exts        []string
+	workingDir  string
 )
 
 type SyntaxHighlight string
@@ -101,7 +107,7 @@ var rootCmd = &cobra.Command{
 				//os.Exit(1)
 			}
 
-			workingDir := filepath.Base(".")
+			workingDir = filepath.Base(".")
 			// Initialize tf -- NOT terraform init
 			tf, err := tfexec.NewTerraform(workingDir, execPath)
 			if err != nil {
@@ -116,105 +122,87 @@ var rootCmd = &cobra.Command{
 			//}
 
 			//the plan file
-			planPath := viper.GetString("planFile")
+			planPath = viper.GetString("planFile")
 			planOpts := []tfexec.PlanOption{
 				// terraform plan --out planPath (plan.out)
 				tfexec.Out(planPath),
 			}
 
-			exts := []string{".tf", ".tofu"}
+			exts = []string{".tf", ".tofu"}
 			files := checkFilesByExtension(workingDir, exts)
 			// we check to see if there are tf or tofu files in the current working directory. If not, we don't call tf.plan
 			if files {
 				// terraform plan -out plan.out -no-color
-				hasChanges, err := tf.Plan(context.Background(), planOpts...)
+				_, err := tf.Plan(context.Background(), planOpts...)
 				if err != nil {
 					// binary defined. .tf or .tofu files exist. Still errors. Show me the error
 					log.Println(bold(red("Terraform returned the following error: ")), err)
 					// We need to exist on this error. tf.Plan actually returns status 1 -- maybe some day we can intercept it or have awareness that it was returned.
 					os.Exit(1)
 				}
-				// if tf.Plan has no changes, hasChanges is empty or false
-				if hasChanges {
-					// This is the actual plan output from terraform plan -out plan.out -no-color
 
-					planStr, err = tf.ShowPlanFileRaw(context.Background(), planPath)
-					if err != nil {
-						log.Fatal("error internally attempting to create the human-readable Plan: ", err)
-					}
-
-					if Verbose {
-						log.Println((planStr))
-					}
-
-					//fmt.Printf("plan output: %s", planStr)
-					mdParam := viper.GetString("mdFile")
-
-					planMd, err := os.Create(mdParam)
-					if err != nil {
-						log.Fatalf("failed to create Markdown: %s", err)
-					}
-					// Close the file when we're done with it
-					defer planMd.Close()
-
-					// This has the plan wrapped in a code block in Markdown
-					planBody := md.NewMarkdown(os.Stdout).CodeBlocks(md.SyntaxHighlight(SyntaxHighlightTerraform), planStr)
-					if err != nil {
-						log.Fatalf("error generating plan Markdown: %s", err)
-					}
-
-					// NewMarkdown returns io.Writer
-					fmt.Fprintf(&sb, "\n%s\n", planBody)
-
-					// This turns NewMarkdown io.Writer into a String, which .Details expects
-					sbPlan := sb.String()
-
-					// This is what creates the final document (`mdoutfile`) plmd here could possibly be os.Stdout one day
-					md.NewMarkdown(planMd).Details("Terraform Plan", sbPlan).Build()
-
-				} else {
-
-					// hasChanges is false, so returns false. We mock a response mirroring Terraform's behavior to still create the Markdown
-					planStr = "No Changes. Your Infrastructure matches the configuration."
-
-					mdParam := viper.GetString("mdFile")
-
-					planMd, err := os.Create(mdParam)
-					if err != nil {
-						log.Fatalf("Failed when trying to create Markdown file: %s", err)
-					}
-					defer planMd.Close()
-
-					// This wraps the above string in a code block in Markdown
-					planBody := md.NewMarkdown(os.Stdout).CodeBlocks(md.SyntaxHighlight(SyntaxHighlightTerraform), planStr)
-
-					// Markdown returns an io.Writer
-					fmt.Fprintf(&sb, "\n%s'\n", planBody)
-
-					// This turns NewMarkdown io.Writer into a String, which .Details expects
-					sbPlan := sb.String()
-
-					// This is what creates the final document (`mdoutfile`) plmd here could possibly be os.Stdout one day
-					md.NewMarkdown(planMd).Details("Terraform Plan", sbPlan).Build()
-
-					log.Println(bold(green("No changes."), "Your infrastructure matches the configuration."))
+				planStr, err = tf.ShowPlanFileRaw(context.Background(), planPath)
+				if err != nil {
+					log.Fatal("error internally attempting to create the human-readable Plan: ", err)
 				}
-				if _, err := os.Stat(planPath); err == nil {
-					log.Printf("%s was created", planPath)
+
+				if Verbose {
+					log.Println((planStr))
+				}
+
+				//fmt.Printf("plan output: %s", planStr)
+				mdParam := viper.GetString("mdFile")
+
+				planMd, err := os.Create(mdParam)
+				if err != nil {
+					log.Fatalf("failed to create Markdown: %s", err)
+				}
+				// Close the file when we're done with it
+				defer planMd.Close()
+
+				// This has the plan wrapped in a code block in Markdown
+				planBody := md.NewMarkdown(os.Stdout).CodeBlocks(md.SyntaxHighlight(SyntaxHighlightTerraform), planStr)
+				if err != nil {
+					log.Fatalf("error generating plan Markdown: %s", err)
+				}
+
+				// NewMarkdown returns io.Writer
+				fmt.Fprintf(&sb, "\n%s\n", planBody)
+
+				// This turns NewMarkdown io.Writer into a String, which .Details expects
+				sbPlan = sb.String()
+
+				// This is what creates the final document (`mdoutfile`) plmd here could possibly be os.Stdout one day
+				md.NewMarkdown(planMd).Details("Terraform Plan", sbPlan).Build()
+
+				if _, err := os.Stat(mdParam); err == nil {
+					log.Printf("Markdown file %s was created.", mdParam)
 
 				} else if errors.Is(err, os.ErrNotExist) {
 
-					// Apparently the binary exists, tf.Plan shit the bed and didn't tell us
-					log.Fatalf("%s was not created", planPath)
+					//
+					log.Fatalf("Markdown file %s was not created.", mdParam)
 
 				} else {
 
-					// I'm only human. NFC how you got here. I hope to never have to find out
-					log.Printf("If you see this error message, please open a bug. Error Code: TPE002. Error: %s", err)
+					// I'm only human. NFC how you got here. I hope to never have to find out.
+					log.Printf("If you see this error message, please open a bug. Error Code: TPE003. Error: %s", err)
 				}
+			}
+
+			// Checking to see if plan file was created.
+			if _, err := os.Stat(planPath); err == nil {
+				log.Printf("Plan file %s was created.", planPath)
+
+			} else if errors.Is(err, os.ErrNotExist) {
+
+				// Apparently the binary exists, tf.Plan shit the bed and didn't tell us.
+				log.Fatalf("Plan file %s was not created.", planPath)
 
 			} else {
-				log.Fatalf("No Terraform files found. Please run this in a directory with Terraform files present.")
+
+				// I'm only human. NFC how you got here. I hope to never have to find out.
+				log.Printf("If you see this error message, please open a bug. Error Code: TPE002. Error: %s", err)
 			}
 
 		} else if args[0] == "-" {
@@ -228,30 +216,46 @@ var rootCmd = &cobra.Command{
 			if Verbose {
 				fmt.Printf("plan output: %s", planStr)
 			}
+			fmt.Printf("Plan output: %s", planStr)
 
-			planmd := viper.GetString("mdFile")
+			mdParam = viper.GetString("mdFile")
 
-			plmd, err := os.Create(planmd)
+			planMd, err := os.Create(mdParam)
 			if err != nil {
 				log.Fatalf("failed to create Markdown: %s", err)
 			}
 			// Close the file when we're done with it
-			defer plmd.Close()
+			defer planMd.Close()
 
 			// This has the plan wrapped in a code block in Markdown
-			plbody := md.NewMarkdown(os.Stdout).CodeBlocks(md.SyntaxHighlight(SyntaxHighlightTerraform), planStr)
+			planBody := md.NewMarkdown(os.Stdout).CodeBlocks(md.SyntaxHighlight(SyntaxHighlightTerraform), planStr)
 			if err != nil {
 				log.Fatalf("error generating plan Markdown: %s", err)
 			}
 
 			// NewMarkdown returns io.Writer
-			fmt.Fprintf(&sb, "\n%s\n", plbody)
+			fmt.Fprintf(&sb, "\n%s\n", planBody)
 
 			// This turns NewMarkdown io.Writer into a String, which .Details expects
-			sbplan := sb.String()
+			sbPlan := sb.String()
 
 			// This is what creates the final document (`mdoutfile`) plmd here could possibly be os.Stdout one day
-			md.NewMarkdown(plmd).Details("Terraform Plan", sbplan).Build()
+			md.NewMarkdown(planMd).Details("Terraform Plan", sbPlan).Build()
+
+			// Checking to see if Markdown file was created.
+			if _, err := os.Stat(mdParam); err == nil {
+				log.Printf("Markdown file %s was created.", mdParam)
+
+			} else if errors.Is(err, os.ErrNotExist) {
+
+				//
+				log.Fatalf("Markdown file %s was not created.", mdParam)
+
+			} else {
+
+				// I'm only human. NFC how you got here. I hope to never have to find out.
+				log.Printf("If you see this error message, please open a bug. Error Code: TPE003. Error: %s", err)
+			}
 		}
 	},
 }
@@ -272,7 +276,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.Flags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tp.toml)")
+	rootCmd.Flags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tp.toml, can also exist in your project's root directory.)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -313,17 +317,20 @@ func initConfig() {
 	if flagNoColor {
 		color.NoColor = true // disables colorized output
 	}
+
 	// Validate that required 'binary' parameter is set
 	b := viper.IsSet("binary")
 	if !b {
 		log.Fatal(bold(red("Attention! Missing Parameter: "), bold("'binary':"), " (type: string) is not defined in the config file. The value of the binary parameter should be either 'tofu' or 'terraform'. This binary is expected to exist on your $PATH.\n"))
 	}
-	// Check to see if required 'planFile' is set
+
+	// Check to see if required 'planFile' parameter is set
 	o := viper.IsSet("planFile")
 	if !o {
 		log.Fatal(bold(red("Attention! Missing Parameter: "), bold("'planFile':"), " (type: string) is not defined in the config file. This is the name of the plan's output file that will be created by `gh tp`.\n"))
 	}
-	// Check to see if required 'mdFile' is set
+
+	// Check to see if required 'mdFile' parameter is set
 	m := viper.IsSet("mdFile")
 	if !m {
 		log.Fatal(bold(red("Attention! Missing Parameter: "), bold("'mdFile':"), " (type: string) is not defined in the config file. This is the name of the Markdown file that will be created by `gh tp`.\n"))
